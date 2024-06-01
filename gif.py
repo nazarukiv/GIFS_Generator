23e3b09e00e638ba1f9572b6f1007edfd2b3095e
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-06-01 14:21:12 +0100

    Completed fully functional program implementation

---
+++ gif.py
@@ -0,0 +1,28 @@
+import imageio
+import os
+import imageio_ffmpeg as ffmpeg
+
+ffmpeg_path = '/opt/homebrew/bin/ffmpeg'
+
+os.environ["IMAGEIO_FFMPEG_EXE"] = ffmpeg_path
+
+clip = os.path.abspath("/Users/ivan/Desktop/IMG_5361.mov")
+
+def gifMaker(inputPath, targetFormat):
+    outputPath = os.path.splitext(inputPath)[0] + targetFormat
+
+    print(f'Converting {inputPath} \n to {outputPath}')
+
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(outputPath, fps=fps)
+
+    for i, image in enumerate(reader):
+        writer.append_data(image)
+        print(f"Frame {i}")
+
+    print("Done!")
+    writer.close()
+
+gifMaker(clip, '.gif')
